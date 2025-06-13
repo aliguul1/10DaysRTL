a223fb3d163c2a94c6b7637772d8c0adbb53b037
aliguul1 <45958458+aliguul1@users.noreply.github.com>
2025-06-14 00:18:50 +0200

    Update day1.sv
    added module

--- day1.sv
+++ day1.sv
@@ -1,1 +1,11 @@
-
+// Design and verify a 2:1 mux
+// Design and verify a 2:1 mux
+module day1 (
+  input   wire [7:0]    a_i   - First leg of the mux
+  input   wire [7:0]    b_i   - Second leg of the mux
+  input   wire          sel_i - Mux select
+  output  wire [7:0]    y_o   - Mux output
+);
+  assign y_o = sel_i ? a_i : b_i;
+end module;
+    
